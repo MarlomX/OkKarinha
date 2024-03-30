5973c754e56ddc38311652ffb736d204ea0bda95
MarlomX <marlomasriel@gmail.com>
2024-03-30 16:11:57 -0300

    Inicio_CRUD

--- src/main/java/Conexao/ConectionFabric.java
+++ src/main/java/Conexao/ConectionFabric.java
@@ -10,8 +10,8 @@ public class ConectionFabric {
 
 	private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 	private static final String URL = "jdbc:mysql://localhost:3306/mecanica";
-	private static final String USER = "root";
-	private static final String PASS = "MarlomRoot2004";
+	private static final String USER = "Usuario";
+	private static final String PASS = "Senha";
 
 	public static Connection getConection() {
 		try {
